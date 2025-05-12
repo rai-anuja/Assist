bca2de42159ea2d2a28a9d0e2558eb43fede4b1a
rai-anuja <anujadec6@gmail.com>
2025-05-12 21:28:24 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from './screens/HomeScreen';
+import AddDeviceScreen from './screens/AddDeviceScreen';
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Add Device" component={AddDeviceScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
